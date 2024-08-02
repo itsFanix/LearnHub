03a1b70e1bd97f1eea3638ae7456585a5a19a7f5
gnyatsikor <fannynyatsikor@gmail.com>
2024-08-02 09:07:11 +0200

    Implement winner Feature

--- ReactApps/tic-tac-toe/src/App.tsx
+++ ReactApps/tic-tac-toe/src/App.tsx
@@ -19,24 +19,30 @@ function Square({value, onSquareClick } : {value: string, onSquareClick : () => 
 }
 
 
-export default function  Board() {
+ function  Board({xIsNext, squares, onPlay} : {xIsNext : Boolean, squares : string[], onPlay: (arg: string[]) => void }) {
 
-  const [xIsNext, setXIsNext] = useState(true)
+  // const [xIsNext, setXIsNext] = useState(true)
 
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  // const [squares, setSquares] = useState(Array(9).fill(null))
+
+  const winner = calculateWinner(squares);
+  let status  ;
+
+  winner? status = "Winner: " + winner: status = "Next player: " + (xIsNext ? "X" : "O")
 
 
   function handleClick(i : number){
 
-    if(squares[i]) return;
+    if(squares[i] || calculateWinner(squares)) return;
     
     const nextSquares = squares.slice()
     xIsNext? nextSquares[i] = "X" :  nextSquares[i] = "O"     //taking turns
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares)
+    //setXIsNext(!xIsNext)
   }
   return (
     <>
+    <div className='status'>{status}</div>
       <div className="board-row">
           <Square value={squares[0]}  onSquareClick={() => handleClick(0)}/>
           <Square value={squares[1]}   onSquareClick={() => handleClick(1)} />
@@ -53,5 +59,51 @@ export default function  Board() {
           <Square value={squares[8]}  onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  )
+}
+
+function calculateWinner(squares: Array<string>) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  for (let i = 0; i < lines.length; i++ ) {
+    const [a,b,c] = lines[i];
+
+    if(squares[a] && squares[a] == squares[b] && squares[a] == squares[c]){
+      return squares[a];
+
+    }
+  }
+
+  return null
+
+}
+
+export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)])
+
+  const currentSquares = history[history.length -1]
+
+ function handlePlay(nextSquares : string[])  {
+
+ }
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board  xIsNext = {xIsNext} squares = {currentSquares} onPlay = {handlePlay}/>
+      </div>
+      <div className='game-Info'>
+        <ol>{}</ol>
+      </div>
+    </div>
   )
 }
