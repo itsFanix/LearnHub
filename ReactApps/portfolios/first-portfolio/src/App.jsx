50411f36a1837cccd9c0b9ad66edac5ca3b60a69
gnyatsikor <fannynyatsikor@gmail.com>
2024-08-07 06:00:00 +0200

    add nav bar + background color

--- ReactApps/portfolios/first-portfolio/src/App.jsx
+++ ReactApps/portfolios/first-portfolio/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-         <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+       <Navbar/>
     </div>
-  )
-}
+  );
+};
 
 export default App
---
+++ ReactApps/portfolios/first-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'  
+
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaTwitterSquare } from 'react-icons/fa'
+//import { FaSquareXTwitter } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+
+
+//rafce to generate reactArrow function
+//import Logo
+
+const Navbar = () => {
+  return (
+   <nav className  = " bg-red-900 mb-20 flex item-center justify-between py-6">
+
+    <div className='flex flex-shrink-0 items-center'>
+        <img src ="" alt =""></img>
+
+    </div>
+        <div  className='m-8 flex items-center justify-center gap-4 text-2xl'>
+            <FaLinkedin />
+            <FaGithub />
+            <FaTwitterSquare/>
+            <FaInstagram />
+        </div>
+   </nav>
+  )
+}
+
+export default Navbar
