4a12ab88446f1ba6ac833ad74d90b9f43d7f2ede
gnyatsikor <fannynyatsikor@gmail.com>
2024-08-15 19:48:02 +0200

    add animation with frame motion

--- ReactApps/portfolios/first-portfolio/src/components/Hero.jsx
+++ ReactApps/portfolios/first-portfolio/src/components/Hero.jsx
@@ -1,24 +1,54 @@
 import React from 'react'
 import { HERO_CONTENT } from '../constants';
 import profilePic from "../assets/kevinRushProfile.png"
+import { motion } from "framer-motion"
 
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay: delay } }
+})
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35 ">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-6 text-6xl font-thin tracking-tight lg:mt-16  lg:text-8xl">Kevin Rush</h1>
-            <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</span>
+            < motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-6 text-6xl font-thin tracking-tight lg:mt-16  lg:text-8xl">
+              Kevin Rush
+            </motion.h1>
 
-            <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>
+              Full Stack Developer
+            </motion.span>
+
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className='my-2 max-w-xl py-6 font-light tracking-tighter'>
+              {HERO_CONTENT}
+            </motion.p>
           </div>
 
         </div>
 
         <div className='w-full lg:w-1/2 lg:p-8'>
           <div className='flex justify-center'>
-            <img src={profilePic} alt="Kevin" />
+            <motion.img 
+    
+             initial = {{x:100, opacity: 0}}
+             animate = {{x:0, opacity :1}}
+             transition={{duration : 1, delay:1.2}}
+            
+            src={profilePic} alt="Kevin" />
           </div>
         </div>
       </div>
