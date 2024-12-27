97a3a21fae560f31c070396e2c3ee00c62b8bfd6
fanny <renatenyatsikor@gmail.com>
2024-12-27 12:11:08 +0100

    Updtate Interface

--- AndroidApps/basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ AndroidApps/basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -1,8 +1,26 @@
 package com.example.inventory.data
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface ItemDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun  insert(item: Item)
+
+    @Update
+    suspend fun  update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    @Query("SELECT * from items WHERE id = :id")
+    fun getItem(id: Int) : Flow<Item>
+
 }
