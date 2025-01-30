edd284afeeb519119bd723c7919df2d85f72874c
fanny <renatenyatsikor@gmail.com>
2025-01-30 23:46:50 +0100

    update

--- ReactApps/todoApp/src/App.tsx
+++ ReactApps/todoApp/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
                                                                                                                                                                         
   return (
-    <div className=' w-full h-screen bg-black'>
+    <div className='w-full h-screen bg-black'>
       <div className='w-1/2 bg-red-500 flex items-center justify-center'>
         <TodoForm/>
       </div>
