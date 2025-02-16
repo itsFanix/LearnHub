31101bd9f4155166f935ced624c3f4110a3a0323
fanny <renatenyatsikor@gmail.com>
2025-02-16 21:04:01 +0100

    add types file

--- ReactApps/todoApp/src/components/TodoItem.tsx
+++ ReactApps/todoApp/src/components/TodoItem.tsx
@@ -1,12 +1,23 @@
 
+/**
+ * 
+ * @returns 
+ * Todo value 
+ */
 
-function TodoItem() {
+import { Task } from "../data/types"
+
+function TodoItem(todo: Task ) {
     return (
         <>
         <div>
-            
+            <h3>todoValue</h3>
+            <button>Edit</button>
+            <button>Remove</button>
         </div>
         </>
     )
 
 }
+
+export default TodoItem
