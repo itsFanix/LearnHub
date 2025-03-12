607e7ce568af863a606b46e9935791c4c15602d6
fanny <renatenyatsikor@gmail.com>
2025-03-12 19:05:57 +0100

    Tutorial done

--- ReactApps/userprofileapp/src/App.tsx
+++ ReactApps/userprofileapp/src/App.tsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import ListGroup from './components/ListGroup'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
+const handleSelectItem = (item: string) => {
+  console.log(item);
+};
 
 function App() {
-  
-  return <div><ListGroup/></div>
+  const [alertVisible, setAlertVisibility] = useState(false);
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  return (
+    <div>
+      {/* <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      /> */}
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Hello Fanny </Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}> My button</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- ReactApps/userprofileapp/src/components/ListGroup.tsx
+++ ReactApps/userprofileapp/src/components/ListGroup.tsx
@@ -1,21 +1,74 @@
-function ListGroup() {
+import { MouseEvent, useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  //(item :string) => void
+
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // let selectedIndex = 0;
+
+  //Hook : a function that allows us to tap into built-in features in react
+  /**
+   *  StateHook : This component can have data or state that will change over time
+   */
+
+  /**
+   * Each component has it own state
+   * arr[0] variable (selectIndex)
+   * arr[1] update function
+   */
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     /**
      * Empty angle brackets we're telling react to use Fragment
-     *
+     *{} render everything dynamically
      * to wrap all these children
+     * In JSX there is no For loop
+     * true && 'data' = data
      */
     <>
-      <h1> List </h1>
+      <h1> {heading} </h1>
+      {items.length === 0 ? <p>No item found</p> : null}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {" "}
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
 
 export default ListGroup;
+
+/**
+ * Conditional rendering
+ * Data via Props : are the inputs to our components
+ * Passing Functions via Props
+ * State vs Props
+ * State : Local variable , Data Managed bya component similar to local variables / Mutable
+ * Props : Input passed to a component Similar to function args / Immutable
+ * Passing Children
+ * Inspecting Components with React Dev Tools : Extension
+ * Building a Button Component
+ */
