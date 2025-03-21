df462a93119b324f3ebfd962ac34cc15e43ca63a
fanny <renatenyatsikor@gmail.com>
2025-03-21 23:24:18 +0100

    Style Counter

--- ReactApps/userprofileapp/src/components/Counter.tsx
+++ ReactApps/userprofileapp/src/components/Counter.tsx
@@ -3,13 +3,26 @@ import React, { useState } from "react";
 const Counter = () => {
   const [counterValue, setCounterValue] = useState(0);
   return (
-    <div>
+    <div className="mx-auto my-40 flex max-w-sm items-center justify-center gap-x-4 rounded-xl bg-green-300 p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
       <button
         onClick={() => {
           setCounterValue(counterValue - 1);
         }}
       >
-        Decrease
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
       </button>
       <div>{counterValue}</div>
       <button
@@ -17,7 +30,20 @@ const Counter = () => {
           setCounterValue(counterValue + 1);
         }}
       >
-        Incrrease
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
       </button>
     </div>
   );
