c20aad1925af24dbb3555da767d32e2b9eb94807
fanny <renatenyatsikor@gmail.com>
2025-03-25 23:37:45 +0100

    Add bg-color to main component

--- ReactApps/todoApp/src/App.tsx
+++ ReactApps/todoApp/src/App.tsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import { TodoForm } from './components/TodoForm'
-import {TodoList} from './components/TodoList'
+import { useState } from "react";
+import "./App.css";
+import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
 
 function App() {
   // const [count, setCount] = useState(0)
-                                                                                                                                                                        
-  return (
-    <div className='w-full h-screen flex '>
-      <div className='w-1/3  bg-green-200 flex items-center justify-center  p-10'>
-        <TodoForm/>
-      </div>
-      <div className='w-2/3  bg-blue-200 flex items-center justify-center '>
-        <TodoList/>
-      </div>
 
+  return (
+    <div className="w-[80vw] mx-auto bg-amber-400 h-screen flex flex-col items-center">
+      <div className="w-1/3  bg-green-200   p-10">
+        <TodoForm />
+      </div>
+      <div className="w-2/3  bg-blue-200  ">
+        <TodoList />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
