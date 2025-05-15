337ec3c6128fe9e4ba49a90ad3982c680fc6a72e
fanny <renatenyatsikor@gmail.com>
2025-05-15 22:35:19 +0200

    update

--- ReactApps/todoApp/src/components/TodoItem.tsx
+++ ReactApps/todoApp/src/components/TodoItem.tsx
@@ -7,7 +7,7 @@
 function TodoItem(todoValue: string) {
   return (
     <>
-      <div className="bg-[url('src/assets/teuku-fadhil-unsplash.jpg')] bg-cover bg-center rounded-lg p-4 flex flex-row  bg-red-200 m-5">
+      <div className=" bg-cover bg-center rounded-lg p-4 flex flex-row  bg-red-200 m-2">
         <h3 className="">{todoValue}</h3>
         <button>Edit</button>
         <button>Remove</button>
