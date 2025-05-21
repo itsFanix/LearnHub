73aac18102085d4da1d3fff6b3b2f64607894b76
fanny <renatenyatsikor@gmail.com>
2025-05-21 22:31:48 +0200

    update

--- ReactApps/todoApp/src/components/TodoItem.tsx
+++ ReactApps/todoApp/src/components/TodoItem.tsx
@@ -7,11 +7,10 @@
 function TodoItem(todoValue: string) {
   return (
     <>
-      <div className=" bg-cover bg-center rounded-lg p-4 flex flex-row  bg-red-200 m-2">
-        <h3 className="">{todoValue}</h3>
-        <button>Edit</button>
-        <button>Remove</button>
-      </div>
+      <h3 className="text-green-200 font-extrabold  text-3xl">{todoValue}</h3>
+      {/* <div className=" bg-cover bg-center rounded-lg p-4 flex flex-row m-2">
+        {/* <button>Edit</button>
+        <button>Remove</button> </div> */}
     </>
   );
 }
