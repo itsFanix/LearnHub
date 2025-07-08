0d062c91a433422daca02bd0cac81de5758335b1
fanny <renatenyatsikor@gmail.com>
2025-07-08 08:48:10 +0200

    set up movie app project

---
+++ ReactApps/ReactMovies/movieHub/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+function Text(){
+  <div>
+    <p>hello world</p>
+  </div>
+}
+export default App
