807c3c017109eee69782a788a44d2e6205af4df3
fanny <renatenyatsikor@gmail.com>
2025-07-17 17:57:28 +0200

    update

--- ReactApps/ReactMovies/movieHub/src/pages/Home.jsx
+++ ReactApps/ReactMovies/movieHub/src/pages/Home.jsx
@@ -1,20 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import MovieCard from "../components/MovieCard"
 import "../css/Home.css"
+import { getPopularMovies, searchMovies } from "../services/api"
 
 
 function Home() {
 
   const [searchQuery, setSearchQuery] = useState("")
 
-   const movies = [
-    {id:1, title: "John Wick", release_date:"2020"},
-    {id:2, title: "Terminator", release_date:"1999"},
-    {id:3, title: "The Matrix", release_date:"1998"},
-    {id:4, title: "random life", release_date:"2020"}
+  //  const movies = [
+  //   {id:1, title: "John Wick", release_date:"2020"},
+  //   {id:2, title: "Terminator", release_date:"1999"},
+  //   {id:3, title: "The Matrix", release_date:"1998"},
+  //   {id:4, title: "random life", release_date:"2020"}
     
-   ] 
+  //  ] 
 
+  //The useEffect allows you to add side effects to your functions or to your components and define when they should run
+   searchMovies
+   getPopularMovies
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {}, [])
     const handleSearch  = (e)  => {
 
       e.preventDefault()
